66f15710c2b18f987e1456fb7feff6ea3b030c77
Hakan CERAN <hakanceran64@gmail.com>
2022-09-15 21:48:40 +0300

    try catch added

--- java101/not_ortalamasi_hesapla/src/Main.java
+++ java101/not_ortalamasi_hesapla/src/Main.java
@@ -5,27 +5,27 @@ public class Main {
         // Degiskenlerin olusturulmasi
         int mat, fizik, kimya, turkce, tarih, muzik;
 
-        // Scannter sinifinin tanimlanmasi
-        Scanner inp = new Scanner(System.in);
+        try (// Scannter sinifinin tanimlanmasi
+        Scanner inp = new Scanner(System.in)) {
+            // Kullanicidan degerleri al.
+            System.out.println("Matematik notunuz: ");
+            mat = inp.nextInt();
 
-        // Kullanicidan degerleri al.
-        System.out.println("Matematik notunuz: ");
-        mat = inp.nextInt();
+            System.out.println("Fizik notunuz: ");
+            fizik = inp.nextInt();
 
-        System.out.println("Fizik notunuz: ");
-        fizik = inp.nextInt();
+            System.out.println("Kimya notunuz: ");
+            kimya = inp.nextInt();
 
-        System.out.println("Kimya notunuz: ");
-        kimya = inp.nextInt();
+            System.out.println("Turkce notunuz:");
+            turkce = inp.nextInt();
 
-        System.out.println("Turkce notunuz:");
-        turkce = inp.nextInt();
+            System.out.println("Tarih notunuz: ");
+            tarih = inp.nextInt();
 
-        System.out.println("Tarih notunuz: ");
-        tarih = inp.nextInt();
-
-        System.out.println("Muzik notunuz: ");
-        muzik = inp.nextInt();
+            System.out.println("Muzik notunuz: ");
+            muzik = inp.nextInt();
+        }
 
         int toplam = (mat + fizik + kimya + turkce + tarih + muzik);
 
