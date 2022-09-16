1ac4a887e0accad3b941ea6e78d8f0a39505ffd6
Hakan CERAN <hakanceran64@gmail.com>
2022-09-17 00:36:35 +0300

    input try added

--- java101/kdv_tutari_hesapla/src/Main.java
+++ java101/kdv_tutari_hesapla/src/Main.java
@@ -10,10 +10,10 @@ public class Main {
     public static void main(String[] args) {
         double tutar, kdvOran, kdvTutar, kdvliTutar;
 
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Tutar giriniz: ");
-        tutar = input.nextDouble();
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.println("Tutar giriniz: ");
+            tutar = input.nextDouble();
+        }
 
         if(tutar > 0 & tutar < 1000){
             kdvOran = 0.18;
