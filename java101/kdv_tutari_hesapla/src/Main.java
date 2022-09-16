84fc56d0fee03b2a04543d7d94a246c2140b8828
Hakan CERAN <hakanceran64@gmail.com>
2022-09-17 00:34:48 +0300

    KDV Tutar Hesaplama

--- java101/kdv_tutari_hesapla/src/Main.java
+++ java101/kdv_tutari_hesapla/src/Main.java
@@ -1,5 +1,33 @@
+/*
+ * @Authors: Hakan CERAN
+ * @Date: 17.09.2022
+ * @Content: KDV Tutarı Hesaplama
+ */
+
+ import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double tutar, kdvOran, kdvTutar, kdvliTutar;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Tutar giriniz: ");
+        tutar = input.nextDouble();
+
+        if(tutar > 0 & tutar < 1000){
+            kdvOran = 0.18;
+        } else {
+            kdvOran = 0.08;
+        }
+
+        kdvTutar = tutar * kdvOran;
+        kdvliTutar = tutar + kdvTutar;
+
+        System.out.println("");
+        System.out.println("Tutar: " + tutar);
+        System.out.println("KDV Oranı: " + kdvOran);
+        System.out.println("KDV Tutarı: " + kdvTutar);
+        System.out.println("KDVli Tutar: " + kdvliTutar);
     }
 }
